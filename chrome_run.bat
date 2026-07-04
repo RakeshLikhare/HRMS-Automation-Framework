f3e18156aefb2d749f2b8adf33185064b54e98e9
RAKESH LIKHARE <rakeshlikhare@gmail.com>
2026-07-04 11:48:23 +0530

    Delete chrome_run.bat

--- chrome_run.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\hp\AppData\Local\Programs\Python\Python312\Scripts\pytest.exe" -v --browser chrome --html=HTMLReports/myreportxyzAbatGit.html "testCases/OrangeAddEmp_test.py" --disable-warnings -m "group11 and sanity" --alluredir="AllureReports"
-
